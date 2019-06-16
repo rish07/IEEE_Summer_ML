c22b3b03ac2adf8120b9fbc215115af110ac9112
TheCreator01 <rishirajsinghchhabra@gmail.com>
2019-06-16 12:58:31 +0530

    Created +ve and -ve alter pair

--- model.py
+++ model.py
@@ -21,7 +21,7 @@ num_classes = 10
 epochs = 50
 
 
-def contrastive_loss(y_true, y_pred):
+def contrastive_loss(y_true, y_pred): #Loss Function
     margin = 1
     sqaure_pred = K.square(y_pred)
     margin_square = K.square(K.maximum(margin - y_pred, 0))
@@ -30,6 +30,26 @@ def contrastive_loss(y_true, y_pred):
 
 def dist(vects):
     x, y = vects
-    sum_square = K.sum(K.square(x - y), axis=1, keepdims=True)
+    sum_square = K.sum(K.square(x - y), axis=1, keepdims=True)  #Euc Dist
     return K.sqrt(K.maximum(sum_square, K.epsilon()))
+
+def eucl_dist_output_shape(shapes):
+    shape1, shape2 = shapes
+    return (shape1[0], 1)
+
+
+def create_pairs(x, digit_indices): #Creation of +ve and -ve alternative pairs
+    pair = []
+    labels = []
+    n = min([len(digit_indices[d]) for d in range(num_classes)]) - 1
+    for d in range(num_classes):
+        for i in range(n):
+            z1, z2 = digit_indices[d][i], digit_indices[d][i + 1]
+            pair += [[x[z1], x[z2]]]
+            inc = random.randrange(1, num_classes)
+            dn = (d + inc) % num_classes
+            z1, z2 = digit_indices[d][i], digit_indices[dn][i]
+            pair += [[x[z1], x[z2]]]
+            labels += [1, 0]
+    return np.array(pair), np.array(labels)
 
