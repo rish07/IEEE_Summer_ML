a4ce29dd3c009771222ccd25e48366eabe0f9b48
TheCreator01 <rishirajsinghchhabra@gmail.com>
2019-06-16 13:07:45 +0530

    Training network

--- model.py
+++ model.py
@@ -108,3 +108,12 @@ distance = Lambda(dist,
                   output_shape=eucl_dist_output_shape)([processed_a, processed_b])
 
 model = Model([input_a, input_b], distance)
+
+
+#Training
+rms = RMSprop()
+model.compile(loss=contrastive_loss, optimizer=rms, metrics=[accuracy])
+model.fit([tr_pairs[:, 0], tr_pairs[:, 1]], tr_y,
+          batch_size=128,
+          epochs=epochs,
+          validation_data=([te_pairs[:, 0], te_pairs[:, 1]], te_y))
