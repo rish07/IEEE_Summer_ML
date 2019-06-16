f13176fe228f575e77fa38c05f2534c87fc43973
TheCreator01 <rishirajsinghchhabra@gmail.com>
2019-06-16 12:52:47 +0530

    Using keras

---
+++ model.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 16 12:49:50 2019
+
+@author: RishiRaj
+"""
+
+from __future__ import absolute_import
+from __future__ import print_function
+import numpy as np
+
+
+import random
+from keras.datasets import mnist
+from keras.models import Model
+from keras.layers import Input, Flatten, Dense, Dropout, Lambda
+from keras.optimizers import RMSprop
+from keras import backend as K
+
+num_classes = 10
+epochs = 50
+
+
+def contrastive_loss(y_true, y_pred):
+    margin = 1
+    sqaure_pred = K.square(y_pred)
+    margin_square = K.square(K.maximum(margin - y_pred, 0))
+    return K.mean(y_true * sqaure_pred + (1 - y_true) * margin_square)
